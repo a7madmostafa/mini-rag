b9bafda3baad526e2e5535755f3601fea6c1fe01
a7madmostafa <a.m_eg@yahoo.com>
2025-12-27 14:04:52 +0200

    tut-004 first

--- src/main.py
+++ src/main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv(".env")
+from routes.base import base_router
 
 app = FastAPI()
-
-@app.get("/welcome")
-def welcome():
-    return {"message": "Welcome to the mini-rag-course application!"}
-
-
+app.include_router(base_router)
